96d0fc2b922f15eea83a5a3c1a8435abe4b3e939
Trsdy <914137150@qq.com>
2022-11-13 00:04:45 +0800

    Update PlanningTokenClass.h

--- PlanningTokenClass.h
+++ PlanningTokenClass.h
@@ -4,39 +4,62 @@
 
 class TechnoClass;
 
-class PlanningNode
-{
-public:
-	DWORD field_0;
-	DWORD field_4;
-	DWORD field_8;
-	DWORD field_C;
-	int field_10;
-	DWORD field_14;
-};
+class PlanningBranchClass;
+// static_assert(sizeof(PlanningBranchClass) == 0x78);
 
-class PlanningTokenClass
+class PlanningMemberClass
 {
 public:
-	void Clear()
-	{
-		JMP_THIS(0x636310);
-	}
+	TechnoClass* Owner;
+	DWORD Packet;
+	int field_8;
+	char field_C;
+};
+static_assert(sizeof(PlanningMemberClass) == 0x10);
+
+class PlanningNodeClass
+{
+public:
+	static constexpr constant_ptr<DynamicVectorClass<PlanningNodeClass*>, 0xAC4B30u> const Unknown1 {};
+	static constexpr constant_ptr<DynamicVectorClass<PlanningNodeClass*>, 0xAC4C18u> const Unknown2 {};
+	static constexpr constant_ptr<DynamicVectorClass<PlanningNodeClass*>, 0xAC4C98u> const Unknown3 {};
+
+	static constexpr reference<bool, 0xAC4CF4u> const PlanningModeActive {};
 
 	//===========================================================================
 	//===== Properties ==========================================================
 	//===========================================================================
 
 public:
-	TechnoClass *OwnerUnit;
-	DynamicVectorClass<PlanningNode *> PlanningNodes;
 
-	DECLARE_PROPERTY_ARRAY(DWORD, unknown, 0x1C);
+	DynamicVectorClass<PlanningMemberClass*> PlanningMembers;
+	int field_18;
+	bool field_1C;
+	DynamicVectorClass<PlanningBranchClass*> PlanningBranches;
+	//...
+};
+
+class PlanningTokenClass
+{
+public:
+	static constexpr constant_ptr<DynamicVectorClass<PlanningTokenClass*>, 0xAC4C78u> const Array {};
+
+	//===========================================================================
+	//===== Properties ==========================================================
+	//===========================================================================
+
+public:
+	TechnoClass* OwnerUnit;
+	DynamicVectorClass<PlanningNodeClass*> PlanningNodes;
+	bool field_1C;
+	bool field_1D;
+
+	DECLARE_PROPERTY_ARRAY(DWORD, unknown_20_88, 0x1B);
 
 	int field_8C;
 	int ClosedLoopNodeCount;
 	int StepsToClosedLoop;
-
-	DECLARE_PROPERTY(DWORD, field_98);
+	bool field_98;
+	bool field_99;
 };
 static_assert(sizeof(PlanningTokenClass) == 0x9C);
